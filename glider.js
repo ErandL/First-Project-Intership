c50a0b29fa3d0559b1795de6505e4e80543c1ffd
ErandL <erand.tik@gmail.com>
2025-02-26 12:55:24 +0100

    Add images for product display and enhance Glide.js configuration

--- glider.js
+++ glider.js
@@ -1,34 +1,34 @@
-const glide = new Glide('.glide', {
-    type: 'carousel',
-    startAt: 3,
-    perView: 5,
-    focusAt: 'center',
-    breakpoints: {
-        1600: {
-            perView: 4, 
-            },
-     1200: {
-    perView: 3, 
+const glide = new Glide(".glide", {
+  type: "carousel",
+  startAt: 3,
+  perView: 5,
+  focusAt: "center",
+  breakpoints: {
+    1600: {
+      perView: 4,
     },
-     800: {
-        perView: 2,
-      },
-      600: {
-        perView: 1,
-      },
+    1200: {
+      perView: 3,
     },
+    800: {
+      perView: 2,
+    },
+    600: {
+      perView: 1,
+    },
+  },
+});
+
+glide.on(["mount.after", "run"], () => {
+  // Remove the ID from all slides first
+  document.querySelectorAll(".glide__slide").forEach((slide) => {
+    slide.removeAttribute("id");
   });
-  
-  glide.on(['mount.after', 'run'], () => {
-    // Remove the ID from all slides first
-    document.querySelectorAll('.glide__slide').forEach(slide => {
-      slide.removeAttribute('id');
-    });
-  
-    // Add the ID to the focused slide
-    const currentIndex = glide.index;
-    const slides = document.querySelectorAll('.glide__slide');
-    slides[currentIndex].setAttribute('id', 'active-slide');
-  });
-  
-  glide.mount();
+
+  // Add the ID to the focused slide
+  const currentIndex = glide.index;
+  const slides = document.querySelectorAll(".glide__slide");
+  slides[currentIndex].setAttribute("id", "active-slide");
+});
+
+glide.mount();
